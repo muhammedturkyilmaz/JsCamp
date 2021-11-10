1b5e37d4acdfe31bcd054880dc93a44d830b131a
muhammedturkyilmaz <muhammed.turkyilmaz29@gmail.com>
2021-11-10 13:26:15 +0300

    10 Kasım Commit 2

---
+++ src/components/userComponent.js
@@ -0,0 +1,25 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/loger.js"
+import User from "../models/user.js"
+import UserService from "../services/userService.js"
+
+console.log("User component yüklendi")
+
+let logger1 = new MongoLogger()
+let userService = new UserService(logger1)
+
+let user1 = new User(1,"Muhammed","Türkyılmaz","Kocaeli")
+let user2 = new User(2,"Engin","Demiroğ","Ankara")
+userService.add(user1)
+userService.add(user2)
+
+console.log(userService.list())
+
+console.log(userService.getById(1))
+
+
+
+let customer = {id:1, firstName:"Muhammed"}
+
+//prototyping
+customer.lastName = "Türkyılmaz"
+console.log(customer.lastName)
